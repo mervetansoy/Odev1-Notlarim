8fa65694b16a59d6a627383e2cfac3505663d996
Merve Tansoy <157800967+mervetansoy@users.noreply.github.com>
2024-02-01 21:25:14 +0300

    Odev1-Notlarim

---
+++ deneme/deneme/Form3.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace deneme
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.stokDataSet = new deneme.stokDataSet();
+            this.urunBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.urunTableAdapter = new deneme.stokDataSetTableAdapters.urunTableAdapter();
+            this.urunidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urunAdıDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.urunKgDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.depoNoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stokDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.urunidDataGridViewTextBoxColumn,
+            this.urunAdıDataGridViewTextBoxColumn,
+            this.urunKgDataGridViewTextBoxColumn,
+            this.depoNoDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.urunBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 186);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(629, 252);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(103, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(210, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(103, 103);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // stokDataSet
+            // 
+            this.stokDataSet.DataSetName = "stokDataSet";
+            this.stokDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // urunBindingSource
+            // 
+            this.urunBindingSource.DataMember = "urun";
+            this.urunBindingSource.DataSource = this.stokDataSet;
+            // 
+            // urunTableAdapter
+            // 
+            this.urunTableAdapter.ClearBeforeFill = true;
+            // 
+            // urunidDataGridViewTextBoxColumn
+            // 
+            this.urunidDataGridViewTextBoxColumn.DataPropertyName = "Urunid";
+            this.urunidDataGridViewTextBoxColumn.HeaderText = "Urunid";
+            this.urunidDataGridViewTextBoxColumn.Name = "urunidDataGridViewTextBoxColumn";
+            this.urunidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // urunAdıDataGridViewTextBoxColumn
+            // 
+            this.urunAdıDataGridViewTextBoxColumn.DataPropertyName = "UrunAdı";
+            this.urunAdıDataGridViewTextBoxColumn.HeaderText = "UrunAdı";
+            this.urunAdıDataGridViewTextBoxColumn.Name = "urunAdıDataGridViewTextBoxColumn";
+            // 
+            // urunKgDataGridViewTextBoxColumn
+            // 
+            this.urunKgDataGridViewTextBoxColumn.DataPropertyName = "UrunKg";
+            this.urunKgDataGridViewTextBoxColumn.HeaderText = "UrunKg";
+            this.urunKgDataGridViewTextBoxColumn.Name = "urunKgDataGridViewTextBoxColumn";
+            // 
+            // depoNoDataGridViewTextBoxColumn
+            // 
+            this.depoNoDataGridViewTextBoxColumn.DataPropertyName = "DepoNo";
+            this.depoNoDataGridViewTextBoxColumn.HeaderText = "DepoNo";
+            this.depoNoDataGridViewTextBoxColumn.Name = "depoNoDataGridViewTextBoxColumn";
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stokDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.urunBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private stokDataSet stokDataSet;
+        private System.Windows.Forms.BindingSource urunBindingSource;
+        private stokDataSetTableAdapters.urunTableAdapter urunTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urunidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urunAdıDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn urunKgDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn depoNoDataGridViewTextBoxColumn;
+    }
+}
---
+++ deneme/deneme/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace deneme
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            // TODO: Bu kod satırı 'stokDataSet.urun' tablosuna veri yükler. Bunu gerektiği şekilde taşıyabilir, veya kaldırabilirsiniz.
+            this.urunTableAdapter.Fill(this.stokDataSet.urun);
+
+        }
+    }
+}
